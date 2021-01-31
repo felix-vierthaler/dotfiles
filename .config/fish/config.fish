d59dd16c09ef102975cc25ca00ad3cf81a4bf15e
felix-vierthaler <felix.vierthaler+dev@mailbox.org>
2021-01-31 21:44:32 +0100

    fish config added

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+#aliases
+alias v "nvim"
+alias config "git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+#oh my fish setup
+set -g theme_nerd_fonts yes
+set theme_color_scheme gruvbox
+
+#vim key bindings
+fish_vi_key_bindings
+
+#no right promp
+function fish_right_prompt
+end
+
+# No greeting when starting an interactive shell.
+function fish_greeting
+end
+
+#start neofetch
+neofetch
