8e5c0a26c28992d1388c2c0b517d1ad7905d350c
felix-vierthaler <felix.vierthaler+dev@mailbox.org>
2021-02-08 19:05:29 +0100

    fish aliases added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 #aliases
 alias v "nvim"
 alias config "git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+alias ls "ls --color=auto"
+alias lsl "ls --color=auto -la"
 
 #oh my fish setup
 set -g theme_nerd_fonts yes
